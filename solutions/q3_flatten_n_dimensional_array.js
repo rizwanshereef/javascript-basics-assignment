86135ef0c17c2cf0f319241aa1a3d79235594df2
rsrizwan77 <rsrizwan77@gmail.com>
2020-09-28 14:09:24 +0530

    updated code

--- solutions/q3_flatten_n_dimensional_array.js
+++ solutions/q3_flatten_n_dimensional_array.js
@@ -1,35 +1,23 @@
 /* Write a Program to Flatten a given n-dimensional array */
 
-const makeArrayFlat = (flatArray,array) => {
-	for(let i=0;i<array.length;i+1){
-		if(array[i] instanceof Array){
-			makeArrayFlat(flatArray,array[i]);
-		}
-		else{
-			flatArray.push(array[i]);
-		}
-	}
-}
-
 const flatten = (input) => {
-	// Write your code here
-
-	let flatArray = new Array();
-	if(typeof input === 'string'){
-		flatArray = null;
-	}
-	else{
-		for(let i=0;i<input.length;i+1){
-			if(input[i] instanceof Array){
-				makeArrayFlat(flatArray,input[i]);
-			}
-			else{
-				flatArray.push(input[i]);
-			}
+	Array.prototype.flatten = (array) => {
+		const newAr = [];
+		const flat = (array) => {
+			array.map(i => {
+				Array.isArray(i) ? flat(i) : newAr.push(i)
+			})
 		}
-	}
-	return flatArray;
+		flat(input);
+		return newAr;
+	};
+	const result = input.flatten();
+	console.log(result);
+	return result;
 };
+
+flatten([1, [2, 3], [[4], [5]]]);
+
 /* For example,
 INPUT - flatten([1, [2, 3], [[4], [5]])
 OUTPUT - [ 1, 2, 3, 4, 5 ]
